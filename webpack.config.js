f6a3bd20fb15d5d6fea603ed54bec2ec99812637
xing.luo <xing.luo@txtechnology.com.cn>
2018-02-07 15:05:57 +0800

    用webpack重新搭建react

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path')
+module.exports = {
+    entry: './main.js', // 入口文件路径
+    output: {
+        //path是nodeJS的一个基础模块，这里用来获取绝对路径
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3333
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, // babel 转换为兼容性的 js
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'latest']
+                }
+            }
+        ]
+    }
+}
